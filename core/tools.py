4e39c30b2955ab6e25f24ab37e4c86454e9735ef
Rui Pu <ruipu@RuideMacBook-Pro.local>
2025-07-28 18:34:09 +0800

    update ui

--- core/tools.py
+++ core/tools.py
@@ -5,10 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def fetch_content(urls: List[str], client: TavilyClient):
     content = client.extract(urls=urls)["results"]
-    raw_contents = [(item["url"]+":\n"+item["raw_content"][:1000]) for item in content]
+    raw_contents = [
+        (item["url"] + ":\n" + item["raw_content"][:1000]) for item in content
+    ]
     return "\n".join(raw_contents)
+
 
 def web_search(query: str):
     """Search Information from the web based on the query"""
@@ -16,7 +20,7 @@ def web_search(query: str):
     results = client.search(query=query, max_results=1)["results"]
     related_urls = [result["url"] for result in results]
     return fetch_content(related_urls, client)
-    
+
 
 if __name__ == "__main__":
     query = "What is the latest trend in AI?"
